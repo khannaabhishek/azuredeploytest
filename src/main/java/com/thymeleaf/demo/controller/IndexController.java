f0e66b4c230d86efe1bb05a7037a66d8c9e2b4cc
Abhishek Khanna <Abhishek.Khanna@kp.org>
2021-04-12 12:38:58 -0700

    Updated code to dynamically use UserName

--- src/main/java/com/thymeleaf/demo/controller/IndexController.java
+++ src/main/java/com/thymeleaf/demo/controller/IndexController.java
@@ -1,14 +1,17 @@
 package com.thymeleaf.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
 
 	@RequestMapping({"","/","/index"})
-	public String getIndexPage() {
+	public String getIndexPage(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		System.out.println("Recevied GET request....");
+		model.addAttribute("name", name);
 		return "index";
 	}
 }
