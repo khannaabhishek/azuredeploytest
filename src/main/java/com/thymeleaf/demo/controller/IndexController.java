98c7af04e8ed7b33dfbaa58a65938135d09c43de
Abhishek Khanna <Abhishek.Khanna@kp.org>
2021-04-14 17:03:33 -0700

    Added JVM parameter

--- src/main/java/com/thymeleaf/demo/controller/IndexController.java
+++ src/main/java/com/thymeleaf/demo/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.thymeleaf.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +9,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class IndexController {
 
+	@Value("${app.location}")
+	String appLocation;
+	
 	@RequestMapping({"","/","/index"})
 	public String getIndexPage(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		System.out.println("Recevied GET request....");
 		model.addAttribute("name", name);
+		model.addAttribute("applocation",appLocation);
 		return "index";
 	}
 }
--- src/test/java/com/thymeleaf/demo/ThymeleafApplicationTests.java
+++ src/test/java/com/thymeleaf/demo/ThymeleafApplicationTests.java
@@ -9,5 +9,9 @@ class ThymeleafApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
+	@Test
+	void testController() {
+	}	
 
 }
